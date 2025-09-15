f6fe5500af57bf3562552d01778fc3fed0d31a36
Lakshya Singla <lakshyasingla2006@gmail.com>
2025-09-15 23:44:32 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cart from './router/Cart'
 import Home from './router/Home'
 import {NavLink,Routes,Route} from 'react-router';
 function App() {
-  
+  // to make the selected option become colored using NavLink
   function getStyle({isActive}){
     return{
       color: isActive ? 'red' : ''
@@ -12,10 +12,12 @@ function App() {
   }
   return(<>
   
+  {/*  NavLink will help to create a link which works without reloading page    */}
   <NavLink to='/About' style={getStyle}>About</NavLink> <br/>
   <NavLink to='/Cart'style={getStyle}>Cart</NavLink> <br/>
   <NavLink to='/Home'style={getStyle}>Home</NavLink>
   
+  {/* This is for providing route to the links created from NavLink in order to call our different components when any click is clicked*/}
   <Routes>
     <Route path='/About' element={<About />} />
     <Route path='/Cart' element={<Cart />} />
